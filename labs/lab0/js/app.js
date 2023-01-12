2a2238e1872e0b5ec7655c7b70a5509653428b46
bluupers <blupineg@iu.edu>
2023-01-12 17:08:00 -0500

    Adding Base Files

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800,600);
+    background(25, 32, 247);
+
+    fill(25, 247, 162);
+    rect(40,50, 120, 12);
+}
+
+function draw() {
+circle(mouseX, mouseY, 10);
+}
