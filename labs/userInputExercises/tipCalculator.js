fead609780637ed69485a479f797b5d3387a998b
bluupers <blupineg@iu.edu>
2023-03-14 13:32:26 -0500

    User Input Tip Calculator
    completed writing code for tipCalculator User Input exercise

--- labs/userInputExercises/tipCalculator.js
+++ labs/userInputExercises/tipCalculator.js
@@ -9,3 +9,14 @@
     //create a variable called totalAmount
         //add tipAmount value and subtotalInput input
     //console.log string "Total: $" + totalAmount variable
+
+let subtotal = document.getElementById("subtotalInput");
+
+function calculateTab() {
+    let subtotalAmount = Number(subtotal.value);
+    let tipAmount = subtotalAmount * 0.2;
+    console.log("Tip: $" + tipAmount);
+
+    let totalAmount = tipAmount + subtotalAmount;
+    console.log ("Total: $" + totalAmount);
+}
