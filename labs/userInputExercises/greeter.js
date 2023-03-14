f27d19d2bf2315eb89d8e40979389aadb1164e6e
bluupers <blupineg@iu.edu>
2023-03-14 13:11:20 -0500

    User Input Greeter
    completed code for user input exercise "greeter"

--- labs/userInputExercises/greeter.js
+++ labs/userInputExercises/greeter.js
@@ -1,4 +1,7 @@
 //link txtName to js with get element by id
-
+let txtName = document.getElementById("txtName");
 //create a function named helloName
     //console.log the string "hello" + the value entered in the input
+function helloName() {
+console.log("hello " + txtName.value);
+}
