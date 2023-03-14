b5bf27a37c483228d368c7e015f98fec7180a624
bluupers <blupineg@iu.edu>
2023-03-14 13:21:54 -0500

    User Input tipCalculator Algorithm
    created tipCalculator algorithm for user input exercises

---
+++ labs/userInputExercises/tipCalculator.js
@@ -0,0 +1,11 @@
+//enable DOM by linking subtotalInput input by id
+
+//create a function
+
+    //create a variable called tipAmount
+         //multiply the value entered by user by .2 
+    //console.log string "Tip: $" + tipAmount variable
+
+    //create a variable called totalAmount
+        //add tipAmount value and subtotalInput input
+    //console.log string "Total: $" + totalAmount variable
