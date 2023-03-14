6c2ffecfa70bd74ef94c79f2df95d48c28898cc4
bluupers <blupineg@iu.edu>
2023-03-14 14:41:01 -0500

    User Input Exercises
    Completed user input exercises programming, lined algorithm up with code to be all pretty and readable.

--- labs/userInputExercises/passwordProtected.js
+++ labs/userInputExercises/passwordProtected.js
@@ -1,11 +1,18 @@
 //enable DOM with get elements by id variables declared
+let usernameTxt = document.getElementById("usernameTxt");
+let passwordTxt = document.getElementById("passwordTxt");
+let resultDiv = document.getElementById("resultDiv");
 
 //create function checkInfo
+function checkInfo() {
     //if statement
+    if(usernameTxt.value=="Username" && passwordTxt.value=="Password") {
         //set argument as boolean if username.value is equal to string "Username"
         //AND password.value is equal to "Password"
         //then update resultDiv to say "Success"
-
+        resultDiv.innerHTML= "Success";
+    } else {
         //ELSE update resultDiv to say "Wrong information"
-
-
+        resultDiv.innerHTML= "Wrong Information";
+    }
+}
--- labs/userInputExercises/tipCalculator.js
+++ labs/userInputExercises/tipCalculator.js
@@ -1,22 +1,16 @@
 //enable DOM by linking subtotalInput input by id
-
-//create a function
-
-    //create a variable called tipAmount
-         //multiply the value entered by user by .2 
-    //console.log string "Tip: $" + tipAmount variable
-
-    //create a variable called totalAmount
-        //add tipAmount value and subtotalInput input
-    //console.log string "Total: $" + totalAmount variable
-
 let subtotal = document.getElementById("subtotalInput");
-
+//create a function
 function calculateTab() {
     let subtotalAmount = Number(subtotal.value);
+    //create a variable called tipAmount
+         //multiply the value entered by user by .2
     let tipAmount = subtotalAmount * 0.2;
+    //console.log string "Tip: $" + tipAmount variable
     console.log("Tip: $" + tipAmount);
-
+    //create a variable called totalAmount
+        //add tipAmount value and subtotalInput input
     let totalAmount = tipAmount + subtotalAmount;
-    console.log ("Total: $" + totalAmount);
+    //console.log string "Total: $" + totalAmount variable
+    console.log("Total: $" + totalAmount);
 }
