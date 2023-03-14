ce0d24caa58ed4b6cfa761c8ec318760c70fddbc
bluupers <blupineg@iu.edu>
2023-03-14 14:26:06 -0500

    User Input Password Protect Algorithm
    Completed the algorithm for user input exercise password protected

---
+++ labs/userInputExercises/passwordProtected.js
@@ -0,0 +1,11 @@
+//enable DOM with get elements by id variables declared
+
+//create function checkInfo
+    //if statement
+        //set argument as boolean if username.value is equal to string "Username"
+        //AND password.value is equal to "Password"
+        //then update resultDiv to say "Success"
+
+        //ELSE update resultDiv to say "Wrong information"
+
+
