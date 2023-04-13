f478c2e37071829b21302f1c190ab3116149e5ee
bluupers <blupineg@iu.edu>
2023-04-13 17:09:39 -0400

    its just average algorithm

---
+++ labs/aprilEleventh/app.js
@@ -0,0 +1,26 @@
+let myAudio = document.getElementById("myAudio");
+let uiHolder = document.getElementById("uiHolder");
+document.body.addEventListener("keydown", playAudio);
+
+for(let i=0; i < 6; i++) {
+    let newElement = document.createElement("button");
+    newElement.innerHTML = 1;
+    newElement.dataset.startPosition = i * 18;
+    newElement.addEventListener("click", playAudio);
+    uiHolder.appendChild(newElement);
+}
+
+function playAudio(event) {
+    myAudio.currentTime= event.target.dataset.startPosition;
+    console.log(event);
+    myAudio.play();
+
+    // setTimeout(stopAudio, 3000);
+    setTimeout(function () {
+        myAudio.pause();
+    }, 3000);
+}
+
+// function stopAudio() {
+//     myAudio.pause();
+// }
---
+++ labs/arrayComputationExercises/average.js
@@ -0,0 +1,31 @@
+/*Create an application that:
+
+Takes in a list of comma-separated numbers from the user
+
+Splits that list into an array of numbers
+
+Converts the string version of the numbers into number versions
+
+Loops through the array and sums up the values
+
+Calculates the average of the array
+
+Displays to the user
+    The average
+    The sum
+Removes the text from the input so the user can type in new numbers*/
+
+
+//split string on commas
+//put pieces into an array
+//convert string to Number
+
+//for loop
+//sum the array up
+    //display that in a div
+//divide the array by its length
+    //display that in a div
+//remove input text
+
+ 
+
