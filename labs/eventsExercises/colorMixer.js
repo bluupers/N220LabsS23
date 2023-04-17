e3d3afe7eaabba462aa8c217c074d14dc822829e
bluupers <blupineg@iu.edu>
2023-04-17 17:16:21 -0400

    sstruggles in programming

--- labs/eventsExercises/colorMixer.js
+++ labs/eventsExercises/colorMixer.js
@@ -19,19 +19,46 @@ let blueOne = document.getElementById("blueOne");
 let blueFive = document.getElementById("blueFive");
 let blueTen = document.getElementById("blueTen");
 
-//declare one changing div for DOM
+//declare divs for DOM
 let resultsDiv = document.getElementById("resultsDiv");
 let colorDiv = document.getElementById("colorDiv");
 
+//declare beginning color values
+let red = 0;
+let green = 0;
+let blue = 0;
 //style color changing div as black to begin with
-resultsDiv.style.backgroundColor = "rgb" + (0, 0, 0);
+resultsDiv.style.backgroundColor = "rgb" + "(0, 0, 0)";
 resultsDiv.style.height = "300px";
-
-//give each button an attribute of the amount they adjust
+resultsDiv.style.width = "300px";
 
 //add an event listener for each button onclick
+redOne.addEventListener("click", itsClicked);
+redFive.addEventListener("click", itsClicked);
+redTen.addEventListener("click", itsClicked);
+greenOne.addEventListener("click", itsClicked);
+greenFive.addEventListener("click", itsClicked);
+greenTen.addEventListener("click", itsClicked);
+blueOne.addEventListener("click", itsClicked);
+blueFive.addEventListener("click", itsClicked);
+blueTen.addEventListener("click", itsClicked);
 
 //create a function for when a button is clicked
+function itsClicked(event) {
     //declare a variable for the attribute data of the button
+    let newRed = Number(event.target.getAttribute.data-red);
+    let newGreen = Number(event.target.getAttribute.data-green);
+    let newBlue = Number(event.target.getAttribute.data-blue);
+    //create equations for the colors
+    newRed = red + newRed;
+    newGreen = green + newGreen;
+    newBlue = blue + newBlue;
+    console.log("newRed: " + newRed + " newGreen: " + newGreen + " newBlue: " + newBlue);
     //create a variable for new div color equivalent to the current rgb value of the div plus the variable value of the custom button attribute
+    //so this holds final data for color change
+    let newColor = "rgb" + "(" + newRed + "," + newGreen + "," + newBlue + ")";
+    resultsDiv.style.backgroundColor = newColor;
+    console.log(newColor);
     //set background color to new div color value
+}
+    
